642ae2e90d316bd1f0d675a8a4c317fe43b510b0
biggeraudience <elmubaraqy10@gmail.com>
2025-05-24 22:15:55 +0100

    user auth fix

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -29,7 +29,7 @@ const admin = (req, res, next) => {
   if (req.user && req.user.role === 'admin') {
     next();
   } else {
-    res.status(403); // Forbidden
+    res.status(403); 
     throw new Error('Not authorized as an admin');
   }
 };
